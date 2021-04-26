999dd9a40f2db6d4b359de6194967613effd1cac
Omar Jarvis <thejarvisgroupllc@gmail.com>
2021-04-26 19:13:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
 var playlist = { DMX: "Slippin"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artistName]
+  return playlist
+}
+
